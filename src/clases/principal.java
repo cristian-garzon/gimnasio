b5a9e5cf059e11c45e19e1cbe3e239653e5e486c
cristian <cristiancamilogarzon@ucundinamarca.edu.co>
2020-04-18 09:22:29 -0500

    conexion a postgresql ya cree mi conexion, y ahi comente, donde pueden crear ustedes la conexion a su base de datos

--- src/clases/principal.java
+++ src/clases/principal.java
@@ -5,7 +5,8 @@ import javax.swing.JOptionPane;
 
 public class principal {
     public static void main(String[] args) {
-        String opcion =(String) JOptionPane.showInputDialog(null,"Seleccione la base de datos a usar","BASE DE DATOS",JOptionPane.QUESTION_MESSAGE, null, new Object[] {"base 1","base 2","base 3"},"Seleccione");
+        String opcion =(String) JOptionPane.showInputDialog(null,"Seleccione la base de datos a usar","BASE DE DATOS",JOptionPane.QUESTION_MESSAGE, null, new Object[] {"postgreSQL","base 2","base 3"},"Seleccione");
         conexion base=new conexion(opcion);
+        base.conectar();
     }
 }
--- src/conexion/conexion.java
+++ src/conexion/conexion.java
@@ -1,20 +1,33 @@
 package conexion;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
 public class conexion {
     String opcion;
     public conexion(String opcion){
         this.opcion=opcion;
     }
     public void conectar(){
-            switch(opcion){
-                case "base 1":
-                    // aca la opcion para conectar la base Cristian
-                    break;
-                case "base 2":
-                    // aca la opcion para conectar la base Sebastian
-                    break;
-                case "base 3":
-                    /// aca, la accion para conectar la base Daniel
-                    break;
+        switch(opcion){
+            case "postgreSQL":
+                String BD="jdbc:postgresql://localhost:5432/gymbd";
+                String user="postgres";
+                String password="1234";
+                try {
+                    Connection cn=DriverManager.getConnection(BD,user,password);
+                    JOptionPane.showMessageDialog(null,"conectada con exito");
+                } catch (SQLException e) {
+                    JOptionPane.showMessageDialog(null, "error al conectar: "+e);
+                }
+                break;
+            case "base 2":
+                // aca la opcion para conectar la base Sebastian
+                break;
+            case "base 3":
+                /// aca, la accion para conectar la base Daniel
+                break;
             }
     }
 
